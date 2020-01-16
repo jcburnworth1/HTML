21ecf729f71ef35d43e540ab7d4b78dde636b0c9
J.C. Burnworth <jc.burnworth@S05010-MBPR.local>
2020-01-15 22:37:02 -0600

    Dog Years

---
+++ Codecademy/Intro_To_Javascript/Dog_Years/app.js
@@ -0,0 +1,25 @@
+// Setting up ages
+let myAge = 10;
+let earlyYears = 2;
+
+// Update earlyYears
+earlyYears *= 10.5;
+
+// myAge - 2
+let laterYears = myAge - 2;
+
+//Update laterYears
+laterYears *= 4;
+
+// Print current values
+console.log(`Early Years: ${earlyYears}`);
+console.log(`Later Years: ${laterYears}`);
+
+// Calculate dogYears
+let myAgeInDogYears = earlyYears + laterYears;
+
+// Name
+let name = 'J.C.';
+
+// Print name and dog years age to console
+console.log(`Name: ${name} - Dog Age ${myAgeInDogYears}`);
