ea24e46e39d59ec8f665c29c953c5edae5b30475
J.C. Burnworth <jc.burnworth@S05010-MBPR.local>
2020-01-16 20:58:48 -0600

    Race Day

--- Codecademy/Intro_To_Javascript/Race_Day/main.js
+++ Codecademy/Intro_To_Javascript/Race_Day/main.js
@@ -1,2 +1,18 @@
 // Variables
 let raceNumber = Math.floor(Math.random() * 1000);
+let earlyRegister = true;
+let runnerAge = 20;
+
+// Control flow for registration
+if (earlyRegister) {
+  raceNumber += 1000;
+}
+
+// Control flow for runner start time
+if (earlyRegister) {
+  console.log(`Runner Start Time: 9:30AM \n Race Number: ${raceNumber}`);
+} else if (runnerAge > 18){
+  console.log(`Runner Start Time: 11:00AM \n Race Number: ${raceNumber}`);
+} else {
+  console.log(`Runner Start Time: 12:30PM \n Race Number: ${raceNumber}`);
+}
